839c056c42d22e72d4987bce2ed0f7033ad0ba71
Emily Violet Sandstrom <esandst@cerf5.computing.clemson.edu>
2020-10-17 17:48:20 -0400

    Added Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -8,13 +8,27 @@ Emily
 #include <fstream> 
 #include <string>
 
+//Purely here for testing, will delete later 
+int main()
+{
+  Player pl;
+  return 0;
+}
+
 //*************** constructors and destructor ****************
 
+int validInputNum (string request, fstream &file, int lower, int upper); 
+string validInputString(string request, fstream& file);
+
 /*
-Constuctor
-Asks for input and makes a file 
+Default constructor 
+Makes a file
+Sets member values based on user input and writes them to file 
+to Call: Player playerObjectName;
+Calls int validInputNum (string request, fstream &file, int lower, int upper) and 
+string validInputString(string request)
  */
-Player::Player()
+Player::Player(void)
 {
   fstream file;
   
@@ -26,20 +40,69 @@ Player::Player()
   file.open(filename.c_str(), ios::out); 
   
   cout << "Please enter player/character name: ";
+  name = validInputString("NAME: ", file); 
   cout << "Please enter character race: ";
+  race = validInputString("RACE: ", file);
   cout << "Please enter character's class: ";
-  cout << "Please enter how many actions character can make per turn: "; 
-
+  playerClass = validInputString("CLASS: ", file); 
+  cout << "Please enter how many actions character can make per turn: ";
+  actPerTurn = validInputNum("NUMBER_OF_ACTIONS_PER_TURN: ", file, 0, 5); 
+  
   cout << "Please enter armor class (AC): ";
+  ac = validInputNum("ARMOR_CLASS: ", file, 0, 30);
   cout << "Please enter character's speed: ";
-  cout << "Please enter character's hit point maximum: "; 
+  speed = validInputNum("SPEED: ", file, 0, 30);
+  cout << "Please enter character's hit point maximum: ";
+  maxHP = validInputNum("HIT_POINT_MAXIUM: ", file, 0, 30);
   
   cout << "Pleaser enter character strength: ";
+  str = validInputNum("STRENGTH: ", file, 0, 30);
   cout << "Pleaser enter character dexterity: ";
+  dex = validInputNum("DEXTERITY: ", file, 0, 30);
   cout << "Pleaser enter character constitution: ";
+  con = validInputNum("CONSTITUTION: ", file, 0, 30);
   cout << "Pleaser enter character intelligence: ";
+  intelligence = validInputNum("INTELLIGENCE: ", file, 0, 30);
   cout << "Pleaser enter character wisdom: ";
+  wis = validInputNum("WISDOM: ", file, 0, 30);
   cout << "Pleaser enter character charisma: ";
+  cha = validInputNum("CHARISMA: ", file, 0, 30);
+
+  currentHP = maxHP;
+  deathSave = 0;
+}
+
+/*
+Recursive input validation for int's 
+makes sure it's an int and within a range [upper, lower] 
+Clears cin and writes to the file 
+Called by default constructor 
+SCRAPPED VALIDATION FOR  TIME
+ */
+int validInputNum (string request, fstream &file, int lower, int upper)
+{
+  int temp;
+  cin >> temp;
+  cin.clear();
+  cin.ignore(9999,'\n');
+  file << request << temp << endl;
+  return temp;
+}
+
+/*
+No input validation is needed for strings currently
+Clears cin, so anything after a whitespace is ignored
+Writes to the file
+Called by default constructor 
+*/
+string validInputString(string request, fstream& file)
+{
+  string temp;
+  cin >> temp;
+  cin.clear();
+  cin.ignore(9999,'\n');
+  file << request << temp << endl;
+  return temp;
 }
 
 /*
