e587b71bd49647e02987647d21923c15d1c31045
johnpturner8 <johnpturner8@gmail.com>
2020-10-17 17:53:20 -0400

    Enemy

--- Enemy.h
+++ Enemy.h
@@ -8,44 +8,68 @@
 #ifndef ENEMY_H_
 #define ENEMY_H_
 #include <string>
+#include <vector>
+using namespace std;
 
 class Enemy {
 public:
-	Enemy(); //Constructor
-	virtual ~Enemy(); //Destructor
+	Enemy(void); //Prompts user for player information
+	Enemy(string filename); //Loads player from file
+	virtual ~Enemy(); //destructor
+	//functions to get basic stats
 
-	//Functions to get basic stats
+	int roll(int dice); //simulates a dice role by generating a random number % dice
+
+	bool hits (int arkRoll); //returns if the attack role hits
+	void takeDamage(int damage); //checks to see player died also
+	bool death (int); //returns true of player dies
+	void getHealed (int health);//Heals player by increasing current HP
+	//Also retests deathSave back to 0
+	//Also keeps it from going above maxHP
+
+	int attack();
+	int attack(int roll, int damage); //dmage includes the modifiers
+	int heal (int health); //heals
+
+	string getFileName();
 	int getStr(); int getDex(); int getCon(); int getInt(); int getWis(); int getCha();
+	int getMaxHP(); int getCurrentHP();
+	int getInitiative();
 	string getName();
 	string getRace();
-	int getInitiative();
+	string getClass();
 
-	void setStr(int); void setDex(int); void setCon(int);
-	void setInt(int); void setWis(int); void setCha(int);
-	void setInitiative(int roll); //Sets the initiative
+	void setStr(int); void setDex(int); void setCon (int);
+        void setInt(int); void setWis(int); void setCha(int);
 
-	int roll(int dice); //Simulates a dice role by generating a random number % dice
-	
-	bool hits(int arkRoll); //Returns if the attack role hits
-	bool takeDamage(int damage); //Checks to see if the player died also 
-	void getHealed(int health); //Heals player by increasing current HP (also prevents from going over max HP)
-	
-	int attack();
-	int attack(int roll, int damage); //Damage includes the modifiers
-	
-	int heal(int health); //Heals
+	void setMaxHP(int); void setCurrentHP(int);
+
+	void setInitiative(int roll); //Sets the iniative
+
+	void addStatus(string); void removeStatus(string); void removeStatus(int);
+	void printStatus(); bool hasStatus(string);
 	
 private:
-	std::string filename;
-	int str, dex, con, intelligence, wis, cha; //Basic ability stats
-	int maxHP, currentHP; //Hit Points
-	int ac; //Armor class
-	int speed; //Speed
-	int initiative; //Initiative for role order
-	int actPerTurn; //How many actions per turn
+	//Info found in player file
+	string filename;
+	
+	string name;
+	string race;
+	string playerClass;
+	int actPerTurn; //how many actions per turn
 
-	string name; //name of enmemy
-	string race; //enemy race
+	int ac; //armor class
+	int speed; //speed
+	int maxHP;
+	
+	int str, dex, con, intelligence, wis, cha; //Basic ability stats
+
+	//Info not in player file
+	int currentHP;
+	int deathSave; //Character dies when this hits 0
+	int initative; //initative for role order
+	vector <string> status;
+
 };
 
 #endif /* ENEMY_H_ */
