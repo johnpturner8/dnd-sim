1f7d0a51c0b6023d40acd4acbc985e46d33e6678
Kevin Le <le5@babbage31.computing.clemson.edu>
2020-10-17 13:51:43 -0400

    hello

--- driver.cpp
+++ driver.cpp
@@ -4,7 +4,44 @@
  *  Created on: Oct 17, 2020
  *      Author: johnp
  */
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main(){
+  int answer;
+  do{
+    cout << "Welcome to DnD Combat Sim" <<endl<<endl
+	 << "[1] Make Encounter\n\t"
+	 << "[2] Modify Party\n\t"
+         << "[3] Load Encounter\n\t"
+         << "[4] Generate Encounter\n\t"
+         << "[5] Quit Program";
+   cout << "\n\nPlease Select What you Want To Do ";
+   cin >> answer; 
+   //For when the user enters invalid characters
+   cin.clear();
+   cin.ignore(9999, '\n');
+
+   switch (answer)
+      {
+      case 1:
+         break; 
+      case 2:
+         break; 
+      case 3:
+         break; 
+      case 4:
+         break; 
+      case 5:
+         break;
+      }
+
+   } while (answer!=5);
+return 0;
 
 }
