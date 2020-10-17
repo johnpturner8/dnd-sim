ed4470aac20e28462aa2559466388908bf842151
Alyssa C Williams <awill36@cerf12.computing.clemson.edu>
2020-10-17 11:30:10 -0400

    Merge branch 'main' of https://github.com/johnpturner8/dnd-sim into main
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ Turn.cpp
@@ -0,0 +1,18 @@
+/*
+ * Turn.cpp
+ *
+ *  Created on: Oct 17, 2020
+ *      Author: johnp
+ */
+
+#include "Turn.h"
+
+Turn::Turn() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Turn::~Turn() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Turn.h
@@ -0,0 +1,17 @@
+/*
+ * Turn.h
+ *
+ *  Created on: Oct 17, 2020
+ *      Author: johnp
+ */
+
+#ifndef TURN_H_
+#define TURN_H_
+
+class Turn {
+public:
+	Turn();
+	virtual ~Turn();
+};
+
+#endif /* TURN_H_ */
