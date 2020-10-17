23b6feca217286f79da014f2630438ec11c71583
Alyssa C Williams <awill36@cerf12.computing.clemson.edu>
2020-10-17 11:18:16 -0400

    Alyssa's changes to Encounter.h

--- Encounter.h
+++ Encounter.h
@@ -8,10 +8,27 @@
 #ifndef ENCOUNTER_H_
 #define ENCOUNTER_H_
 
+#include <Player.h>
+#include <Enemy.h>
+
+#include <vector>
+#include <fstream>
+
 class Encounter {
-public:
-	Encounter();
-	virtual ~Encounter();
+ public:
+  Encounter();
+  Encounter(ifstream);
+  virtual ~Encounter();
+  
+  void removePlayer(Player);
+  void removeEnemy(Enemy);
+  void addPlayer(Player);
+  void addEnemy(Enemy);
+  
+ private:
+  vector<Player> party;
+  vector<Enemy> enemies;
+  
 };
 
 #endif /* ENCOUNTER_H_ */
